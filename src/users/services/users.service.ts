779f258197394969740c30945d8fbfa4b701ef81
henning410 <henning1.weise@web.de>
2023-04-21 18:49:55 +0200

    added hashing with bcrypt

--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateUserDto } from '../user.dto';
 import { User } from '../user.entity';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
@@ -10,9 +11,20 @@ export class UsersService {
     @InjectRepository(User) private readonly userRepository: Repository<User>,
   ) {}
 
-  createUser(createUserDto: CreateUserDto) {
-    const newUser = this.userRepository.create(createUserDto);
-    return this.userRepository.save(newUser);
+  async createUser(createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+    const createdUser = await this.create({
+      ...createUserDto,
+      password: hashedPassword,
+    });
+    createdUser.password = undefined;
+    return createdUser;
+  }
+
+  async create(createUserDto: CreateUserDto) {
+    const newUser = await this.userRepository.create(createUserDto);
+    await this.userRepository.save(newUser);
+    return newUser;
   }
 
   findUsersById(id: number) {
