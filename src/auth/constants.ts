ac6311b81fb31bc223a883f64df0d9f7493bc214
henning410 <henning1.weise@web.de>
2023-04-28 11:49:30 +0200

    just prepared for refresh token

--- src/auth/constants.ts
+++ src/auth/constants.ts
@@ -1,4 +1,7 @@
 export const jwtConstants = {
-  secret:
+  access_token_secret:
     'DO NOT USE THIS VALUE. INSTEAD, CREATE A COMPLEX SECRET AND KEEP IT SAFE OUTSIDE OF THE SOURCE CODE.',
+  access_token_expiration: '900s',
+  refresh_token_secret: 'THIS IS MY SECRET KEY FOR ME REFRESH TOKEN!',
+  refresh_token_expiration: '2630000s',
 };
